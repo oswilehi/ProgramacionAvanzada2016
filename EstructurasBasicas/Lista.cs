8831cb961bb3f1f9df01007b148a8c7de821a02b
bobert-hc <bruiz@healthcare.com>
2016-08-24 08:30:51 -0600

    Listas utilizando Arreglos

---
+++ EstructurasBasicas/Lista.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructurasBasicas
+{
+    public class Lista
+    {
+        private int[] elements;
+        private int count;
+        public int Count { get { return count; } }
+
+        public void Initialize(int size)
+        {
+            count = 0;
+            elements = new int[size];
+        }
+
+        public bool InsertLast(int newElement)
+        {
+            if (count < elements.Length)
+            {
+                elements[count] = newElement;
+                count++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool InsertFirst(int newElement)
+        {
+            if (count == elements.Length)
+                return false;
+
+            for (int i = count; i > 0 ; i--)
+            {
+                elements[i] = elements[i - 1];                
+            }
+            count++;
+            elements[0] = newElement;
+            return true;
+        }
+
+        public bool IsEmpty()
+        {
+            return count == 0;
+        }
+
+        public int FirstElement()
+        {
+            if (count > 0)
+                return elements[0];
+            return 0;
+        }
+
+        public int LastElement()
+        {
+            if (count > 0)
+                return elements[count - 1];
+            else
+                return 0;
+
+        }
+
+    }
+}
---
+++ EstructurasBasicas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructurasBasicas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Lista myList = new Lista();
+            myList.Initialize(10);
+            myList.InsertFirst(40);
+            myList.InsertFirst(20);
+            myList.InsertLast(12);
+            Console.ReadLine();
+        }
+    }
+}
