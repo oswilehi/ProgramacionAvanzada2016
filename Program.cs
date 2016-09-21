04fac9fa0336f88cea4f4b9d9f4c157ca3133971
Yulisa Azurdia <yulyazurdia8@gmail.com>
2016-09-20 21:19:18 -0600

    Program and the class for Recursive Functions
    Prime numbers and palindromes

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosdeRecursividad
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Recursividad ejercicios = new Recursividad();
+
+            string palabra = "";
+            int numero = 0;
+
+            Console.WriteLine("PALÍNDROMOS: ");
+            Console.WriteLine("Ingrese una palabra: ");
+            palabra = Console.ReadLine();
+
+            if (ejercicios.Pal(palabra) == true)
+            {
+                Console.WriteLine("La palabra [" + palabra + "] es palíndromo");
+            }
+            else
+            {
+                Console.WriteLine("La palabra [" + palabra + "] no es palíndromo");
+            }
+
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("NÚMEROS PRIMOS: ");
+            Console.WriteLine("Ingrese un número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (ejercicios.Primo(numero) == true)
+            {
+                Console.WriteLine("El número [" + numero + "] es primo");
+            }
+            else
+            {
+                Console.WriteLine("El número [" + numero + "] no es primo");
+            }
+
+            Console.ReadLine();
+        }     
+    }
+}
---
+++ Recursividad.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolucionParcial1
+{
+    class Recursividad
+    {
+        public bool Pal(string P)
+        {
+            if (P.Length == 1)
+            {
+                return true;
+            }
+            if(P.Length == 2)
+            {
+                if (P[0] == P[1])
+                    return true; 
+            }
+            if (P[0] == P[P.Length - 1])
+                return Pal(P.Substring(1, P.Length - 2));
+
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Prim(int x, int y)
+        {
+            if (y == 1)
+                return true;
+            if (x % y == 0)
+                return false;
+            else
+                return Prim(x, y - 1);
+        }
+        public bool Primo(int x)
+        {
+            return Prim(x, x - 1);
+        }
+    }
+}
