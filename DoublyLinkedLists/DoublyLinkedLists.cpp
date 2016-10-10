37cd715da18fb3b17966ffa3e25b364c1c8e5074
Bobert87 <boris.rp@gmail.com>
2016-10-10 08:44:47 -0600

    Doubly Linked Lists Example

---
+++ DoublyLinkedLists/DoublyLinkedLists.cpp
@@ -0,0 +1,17 @@
+// DoublyLinkedLists.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "DoublyLinkedList.h"
+
+
+int main()
+{
+	DoublyLinkedList* list = new DoublyLinkedList();
+	list->Add(3);
+	list->Add(6);
+	
+    return 0;
+}
+
---
+++ DoublyLinkedLists/Node.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "Node.h"
+
+
+CNode::CNode()
+{
+	value = 0;
+	next = NULL;
+	previous = NULL;
+}
+
+
+CNode::~CNode()
+{
+}
